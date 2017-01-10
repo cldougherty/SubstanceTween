f52c3bff49e1cc81e56d9ca7066f866964e60c3b
Chris <chris.ll.dougherty@gmail.com>
2017-01-09 17:12:39 -0800

    Updated tool to 2.3

---
+++ SubstanceTween v2.3/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+		float yAxisValue = Input.GetAxis("Horizontal 2");
+		if(Camera.current != null)
+		{
+			Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, zAxisValue));
+		}
+
+	
+	}
+}
